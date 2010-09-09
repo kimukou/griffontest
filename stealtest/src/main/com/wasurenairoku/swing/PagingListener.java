f1d1fd0a804a481158a5dfad2450c12904bd04cc
kimukou_26 <kimukou.buzz@gmail.com>
2010-09-09 11:45:13 +0900

    delete not use source

--- stealtest/src/main/com/wasurenairoku/swing/PagingListener.java
+++
@@ -1,6 +0,0 @@
-package com.wasurenairoku.swing;
-import java.util.EventListener;
-
-public interface PagingListener extends EventListener {
-    public abstract void actionPerformed(PagingEvent event);
-}
