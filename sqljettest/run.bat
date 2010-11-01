188a62551744547e95d775e34dbaa8c813e7c924
kimukou_26 <kimukou.buzz@gmail.com>
2010-11-01 17:48:32 +0900

    0.9.2-beta-1 update test

--- sqljettest/run.bat
+++ sqljettest/run.bat
@@ -1,5 +1,5 @@
 set GROOVY_HOME=C:\opt\groovy-1.7.5
-set GRIFFON_HOME=C:\opt\griffon-0.9.1a
+set GRIFFON_HOME=C:\opt\griffon-0.9.2-beta-1
 set JAVA_HOME=c:\opt\jdk
 
 set PATH=%GROOVY_HOME%/bin;%GRIFFON_HOME%/bin;%JAVA_HOME%/bin
