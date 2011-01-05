3a55b1715b39407649f3f7de8dcf79befd64d317
kimukou_26 <kimukou.buzz@gmail.com>
2011-01-05 22:32:57 +0900

    marge

--- fxtest/griffon-app/views/fxtest/FxtestView.groovy
+++ fxtest/griffon-app/views/fxtest/FxtestView.groovy
@@ -1,6 +1,16 @@
 package fxtest
 
-/*
+import griffon.builder.fx.FxBuilder
+
+import javafx.stage.*
+import javafx.scene.*
+import javafx.scene.shape.*
+import javafx.scene.paint.*
+import javafx.scene.control.*
+import javafx.scene.text.*
+
+import org.widgetfx.*
+
 application(title: 'fxtest',
   //size: [320,480],
   pack: true,
@@ -11,29 +21,55 @@ application(title: 'fxtest',
                imageIcon('/griffon-icon-32x32.png').image,
                imageIcon('/griffon-icon-16x16.png').image]) {
     // add content here
-    label('Content Goes Here') // deleteme
-}
+
+
+	tabbedPane(){
+		panel(title:"normal",border: emptyBorder(0)) {
+    	label('Content Goes Here') // deleteme
+		}
+		panel(title:"FXBuilder",border: emptyBorder(0)) {
+			def instance=new FxBuilder().build{
+			    stage(
+			        title: "Sample by FxBuilder of Groovy",
+			        width: 600,
+			        height: 450,
+			        scene: scene(fill: Color.$LIGHTSKYBLUE) {
+			            rectangle(
+			                x: 25, y: 40,
+			                width: 100, height: 50,
+			                fill: Color.$RED
+			            )
+			        }
+			    )
+			}
+			println "instance=${instance.class}/${instance.dump()}"
+			//widget(instance.getWindow())
+		}
+/*
+		panel(title:"WidgetTest",border: emptyBorder(0)) {
+			def builder = new FxBuilder()
+			//builder.registerFactory("Widget",new WidgetFactory(Widget))
+			builder.registerBeanFactory("Widget",org.widgetfx.Widget)
+			def instance2=builder.build{
+				Widget (
+				    width: 100,
+				    height: 100,
+				    visible: true,
+						content: eclipse (
+		          centerX: 50,
+		          centerY: 50,
+		          radiusX: 50,
+		          radiusY: 50,
+		          fill: Color.$RED
+			      )
+				)
+			}
+			//widget(instance2)
+		}
 */
+	}
 
-
-import griffon.builder.fx.FxBuilder
-
-import javafx.stage.*
-import javafx.scene.*
-import javafx.scene.shape.*
-import javafx.scene.paint.*
-
-new FxBuilder().edt {
-    stage(
-        title: "Sample by FxBuilder of Groovy",
-        width: 600,
-        height: 450,
-        scene: scene(fill: Color.$LIGHTSKYBLUE) {
-            rectangle(
-                x: 25, y: 40,
-                width: 100, height: 50,
-                fill: Color.$RED
-            )
-        }
-    )
+	// src/javafx/*.fx
+	new FxBuilder().build(MouseEventSample)
 }
+
