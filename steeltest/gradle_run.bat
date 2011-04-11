9a9d3f9429a04f1c91bf8a70251ffeb8547c7222
kimukou <kimukou.buzz@gmail.com>
2011-04-11 11:44:30 +0900

    gradle 1.0-m2 update

--- steeltest/gradle_run.bat
+++ steeltest/gradle_run.bat
@@ -1,6 +1,6 @@
 set GROOVY_HOME=C:\opt\groovy-1.7.10
 set GRIFFON_HOME=C:\opt\griffon-0.9.2
-set GRADLE_HOME=D:\Tooldev\gradle-1.0-m1
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
 set JAVA_HOME=c:\opt\jdk
 
 set PATH=%GROOVY_HOME%/bin;%GRADLE_HOME%/bin;%JAVA_HOME%/bin;%GRIFFON_HOME%/bin
