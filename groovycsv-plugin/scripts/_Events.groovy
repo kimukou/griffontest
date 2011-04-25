ed7ae17a8cdc0a71b4239939ad4d60b48de758ed
kimukou <kimukou.buzz@gmail.com>
2011-04-25 18:00:33 +0900

    ivy plugin test

--- groovycsv-plugin/scripts/_Events.groovy
+++ groovycsv-plugin/scripts/_Events.groovy
@@ -1,6 +1,8 @@
+/*
 eventCopyLibsEnd = { jardir ->
 		println "==eventCopyLibsEnd(groovycsv)=="
     ant.fileset(dir:"${getPluginDirForName('groovycsv').file}/lib/", includes:"*.jar").each {
         griffonCopyDist(it.toString(), jardir)
     }
 }
+*/
