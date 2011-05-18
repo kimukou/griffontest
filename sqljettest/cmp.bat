5b0dbe2c48251390800bced05c7f4079bc42a060
kimukou <kimukou.buzz@gmail.com>
2011-05-18 18:59:13 +0900

    update problem sqljettest

--- sqljettest/cmp.bat
+++ sqljettest/cmp.bat
@@ -1,6 +1,6 @@
 ::set GROOVY_HOME=C:\opt\groovypp-0.4.116
-set GROOVY_HOME=C:\opt\groovy-1.7.10
-set GRIFFON_HOME=C:\opt\griffon-0.9.2
+set GROOVY_HOME=C:\opt\groovy-1.8.0
+set GRIFFON_HOME=C:\opt\griffon-0.9.3-beta-1
 set JAVA_HOME=c:\opt\jdk
 
 set PATH=%GROOVY_HOME%/bin;%GRIFFON_HOME%/bin;%JAVA_HOME%/bin
