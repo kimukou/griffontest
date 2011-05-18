79120dafe8679bf7ae12fb6f5087746196be0048
kimukou <kimukou.buzz@gmail.com>
2011-05-18 18:39:18 +0900

    griffon 0.9.2-beta-1 update(all yet

--- quartztest/setEnvG9.bat
+++ quartztest/setEnvG9.bat
@@ -1,6 +1,6 @@
 set JAVA_HOME=c:\opt\jdk
-set GROOVY_HOME=C:\opt\Groovy-1.7.10
-set GRIFFON_HOME=C:\opt\griffon-0.9.2
+set GROOVY_HOME=C:\opt\groovy-1.8.0
+set GRIFFON_HOME=C:\opt\griffon-0.9.3-beta-1
 ::set JAVAFX_HOME=C:\opt\javafx121
 
 ::set PATH=%PATH%;%GRIFFON_HOME%/bin;%GROOVY_HOME%/bin;%JAVA_HOME%/bin
