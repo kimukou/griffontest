bd53b2e78d7de8f8eaa26b8b193a3329661935fd
kimukou <kimukou.buzz@gmail.com>
2011-05-20 16:23:42 +0900

    update

---
+++ jdcmtest/eclipse_update.bat
@@ -0,0 +1,11 @@
+set GROOVY_HOME=C:\opt\groovy-1.8.0
+set GRIFFON_HOME=C:\opt\griffon-0.9.3-beta-1
+set JAVA_HOME=c:\opt\jdk
+set USER_HOME=%USERPROFILE%
+echo %USER_HOME%
+
+
+set PATH=%GROOVY_HOME%/bin;%GRIFFON_HOME%/bin;%JAVA_HOME%/bin
+
+call griffon clean
+call griffon eclipse-update
