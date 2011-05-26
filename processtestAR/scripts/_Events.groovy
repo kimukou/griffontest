5d795c78dcce277531c2f1a16da68be3e028bd90
kimukou <kimukou.buzz@gmail.com>
2011-05-26 11:35:16 +0900

    update izpack events

--- processtestAR/scripts/_Events.groovy
+++ processtestAR/scripts/_Events.groovy
@@ -34,17 +34,17 @@ eventCompileEnd = {msg->
   println "==compile end(${msg})=="
   growlNotify("eventCompileEnd")
 
-	libDir  = "${basedir}/lib/native"
-	destDir = "${basedir}/staging/windows/native"
+  libDir  = "${basedir}/lib/native"
+  destDir = "${basedir}/staging/windows/native"
 
   ant.copy(todir: destDir) {
-		fileset(dir: libDir, includes: '*.dll')
-	}
+    fileset(dir: libDir, includes: '*.dll')
+  }
 
-	//libDir  = "${basedir}/lib/libraries/opengl/library"
+  //libDir  = "${basedir}/lib/libraries/opengl/library"
   //ant.copy(todir: destDir) {
-	//	fileset(dir: libDir, includes: '*.dll,*.so')
-	//}
+  //  fileset(dir: libDir, includes: '*.dll,*.so')
+  //}
 
 }
 
@@ -52,8 +52,8 @@ eventCompileEnd = {msg->
 eventPackageStart={msg->
   println "==package start(${msg})=="
 
-	libDir  = "${basedir}/lib/native"
-	//doWithPlatform(platform,libDir)
+  libDir  = "${basedir}/lib/native"
+  //doWithPlatform(platform,libDir)
 }
 
 eventPackageEnd = {msg->
@@ -64,52 +64,52 @@ eventPackageEnd = {msg->
 
 
 doWithPlatform = { platformOs,libDir ->
-		println "copySetting $platformOs"
+    println "copySetting $platformOs"
 
-		if(!platformOs.startsWith('windows'))return
+    if(!platformOs.startsWith('windows'))return
 
-		nativeLibs=["$libDir/dsj.dll",]
-		nativeLibs64=["$libDir/64bit/dsj.dll",]
+    nativeLibs=["$libDir/dsj.dll",]
+    nativeLibs64=["$libDir/64bit/dsj.dll",]
 
-		new File("$libDir/jogl-compat/windows/native").eachFile{
-			nativeLibs<< it.absolutePath
-			nativeLibs64<< it.absolutePath
-		}
+    new File("$libDir/jogl-compat/windows/native").eachFile{
+      nativeLibs<< it.absolutePath
+      nativeLibs64<< it.absolutePath
+    }
 
     def origPlatformOs = platformOs
     if(origPlatformOs.endsWith('64')){
-			 platformOs -= '64'
-		}
+       platformOs -= '64'
+    }
 
-		def rs = buildConfig.griffon.extensions.resources['windows']
-	  if(!rs) {
-	   	 if(origPlatformOs.endsWith('64')){
-				co.nativelibs = nativeLibs64
-			 }
-			 else{
-				co.nativelibs = nativeLibs
-			 }
+    def rs = buildConfig.griffon.extensions.resources['windows']
+    if(!rs) {
+       if(origPlatformOs.endsWith('64')){
+        co.nativelibs = nativeLibs64
+       }
+       else{
+        co.nativelibs = nativeLibs
+       }
        buildConfig.griffon.extensions.resources[origPlatformOs] = co
-		}
-		else{
+    }
+    else{
         if(!rs.nativeLibs) rs.nativeLibs = [] 
-	   	  if(origPlatformOs.endsWith('64'))rs.nativeLibs.addAll(nativeLibs64)
-	   	  else rs.nativeLibs.addAll(nativeLibs)
-		}
+        if(origPlatformOs.endsWith('64'))rs.nativeLibs.addAll(nativeLibs64)
+        else rs.nativeLibs.addAll(nativeLibs)
+    }
 
-		println "rs=${rs.dump()}"
+    println "rs=${rs.dump()}"
 }
 
 
 def copySetting(destDir){
-	println "==copySetting(${destDir})=="
+  println "==copySetting(${destDir})=="
 
-	if(platform.startsWith('windows')==false)return
+  if(platform.startsWith('windows')==false)return
 
 //dsj.dll
-	srcDir =""
-	if(platform.endsWith('64'))srcDir  = "${basedir}/lib/native/64bit"
-	else srcDir  = "${basedir}/lib/native"
+  srcDir =""
+  if(platform.endsWith('64'))srcDir  = "${basedir}/lib/native/64bit"
+  else srcDir  = "${basedir}/lib/native"
 
   ant.mkdir(dir:destDir)
   ant.copy(todir: destDir, overwrite: true ) {
@@ -117,7 +117,7 @@ def copySetting(destDir){
   }
 
 //jogl-compat
-	srcDir  = "${basedir}/lib/jogl-compat/windows/native"
+  srcDir  = "${basedir}/lib/jogl-compat/windows/native"
   ant.copy(todir: destDir, overwrite: true ) {
     fileset(dir: srcDir, includes: '*.dll')
   }
@@ -135,15 +135,15 @@ eventPreparePackageStart={ type->
 
 eventPreparePackageEnd={ type->
   println "==eventPreparePackageEnd [${type}]=="
-	println "sign=${buildConfig.griffon.jars.sign}"
-	println "pack=${buildConfig.griffon.jars.pack}"
+  println "sign=${buildConfig.griffon.jars.sign}"
+  println "pack=${buildConfig.griffon.jars.pack}"
 
   switch(type){
     case "windows":
-			//2011/04/08 kimukou_26 skip jar signature add start
-			buildConfig.griffon.jars.sign = false
-			buildConfig.griffon.jars.pack  = false
-			//2011/04/08 kimukou_26 skip jar signature add end
+      //2011/04/08 kimukou_26 skip jar signature add start
+      buildConfig.griffon.jars.sign = false
+      buildConfig.griffon.jars.pack  = false
+      //2011/04/08 kimukou_26 skip jar signature add end
 
       tmplfile="${basedir}/setting/${griffonAppName}.jsmooth"
       dstfile="${basedir}/target/installer/jsmooth/${griffonAppName}.jsmooth"
@@ -151,14 +151,14 @@ eventPreparePackageEnd={ type->
       break
 
     case "izpack":
-			//2011/04/08 kimukou_26 skip jar signature add start
-			buildConfig.griffon.jars.sign = false
-			buildConfig.griffon.jars.pack  = false
-			//2011/04/08 kimukou_26 skip jar signature add end
+      //2011/04/08 kimukou_26 skip jar signature add start
+      buildConfig.griffon.jars.sign = false
+      buildConfig.griffon.jars.pack  = false
+      //2011/04/08 kimukou_26 skip jar signature add end
 
-	    installerWorkDir = "${projectWorkDir}/installer/izpack"
-  	  binaryDir = installerWorkDir + '/binary'
-  	  installerResourcesDir = installerWorkDir + '/resources'
+      installerWorkDir = "${projectWorkDir}/installer/izpack"
+      binaryDir = installerWorkDir + '/binary'
+      installerResourcesDir = installerWorkDir + '/resources'
 
       copySetting("$binaryDir/lib/windows/native")
       ant.copy( todir: installerResourcesDir, overwrite: true ) {
@@ -190,5 +190,16 @@ eventCreatePackageEnd = { type->
   }
 
   growlNotify("eventCreatePackageEnd(${type})")
+}
+
+import java.text.SimpleDateFormat
+
+eventCreatePackageIzpackEnd = {
+    Date date = new Date()
+    //SimpleDateFormat sdf = new SimpleDateFormat("yyyy'年'MM'月'dd'日'");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy=MM=dd");
+    String tstamp = sdf.format(date)
+    ant.move(file: "${distDir}/izpack/${griffonAppName}-${griffonAppVersion}-installer.jar",
+             tofile: "${distDir}/izpack/${griffonAppName}-${griffonAppVersion}-installer-${tstamp}.jar")
 }
 
